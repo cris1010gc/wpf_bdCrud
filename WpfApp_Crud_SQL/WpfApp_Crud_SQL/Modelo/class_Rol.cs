2aa38634ff4fb150f809dc5cb477044af5f829c8
Cristhian García <cris1010gc@gmail.com>
2023-10-26 16:42:53 -0600

    Se agrega la clase Rol (Modelo) y la  carpeta lista

---
+++ WpfApp_Crud_SQL/WpfApp_Crud_SQL/Modelo/class_Rol.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp_Crud_SQL.Modelo
+{
+    public class class_Rol
+    {   // Atributos 
+        private String nombre;
+        private String descripcion;
+        // Constructor
+        public class_Rol(string NOMBRE, string DESCRIPCION)
+        {
+            this.nombre = NOMBRE;
+            this.descripcion = DESCRIPCION;
+        }
+        // Métodos utilitarios 
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+    }
+}
